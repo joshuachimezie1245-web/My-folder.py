68f5adade35087e635ca6670c0dd20f7c8036499
joshuachimezie1245-web <joshuachimezie1245@gmail.com>
2026-06-25 10:21:56 +0100

    Initial commit

---
+++ My folder.py
@@ -0,0 +1,6 @@
+#program to calculate the average of three number 
+num1 = input("Enter first number")
+num2 = input("Enter second number")
+num3 = input("Enter third number")
+average = ("num1 + num2 +num3") / 3
+print("average")
